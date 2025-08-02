8bbd71b31ab553cda97d05c10e1a20e4e3ea1240
Michał Hrycek <kotyk9921@gmail.com>
2025-08-03 01:25:13 +0200

    Added generating slab blockstates
    Added generating slab blockstates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 package com.mysiupysiu.genmcscript
 
-fun main() {}
+import java.io.File
+
+fun main() {
+    val name = readLine() ?: ""
+    File("$name.json").writeText(readFile().replace("::", name))
+}
+
+fun readFile(): String {
+    val stream = object {}.javaClass.getResourceAsStream("/blockstates/slab.json") ?: throw IllegalArgumentException("Exception: File not found.")
+    return stream.bufferedReader().use { it.readText() }
+}
