d0f159339fb866a452f712eb1c326a96511ae0c2
Michał Hrycek <kotyk9921@gmail.com>
2025-08-03 18:44:48 +0200

    Added ability to enter namespace
    Added ability to enter namespace

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,24 +6,25 @@ fun main() {
     val values = readLine() ?: ""
     val parts = values.trim().split(" ")
 
-    val type = parts[0]
-    val name = parts[1]
+    val namespace = parts[0]
+    val type = parts[1]
+    val name = parts[2]
 
     File("generated/blockstates").mkdirs()
     File("generated/models/block").mkdirs()
     File("generated/models/item").mkdirs()
 
     if (type == "slab") {
-        File("generated/blockstates/${name}_slab.json").writeText(readFile("/blockstates/slab.json").replace("::", name))
-        File("generated/models/block/${name}_slab.json").writeText(readFile("/models/block/slab.json").replace("::", name))
-        File("generated/models/block/${name}_slab_top.json").writeText(readFile("/models/block/slab_top.json").replace("::", name))
-        File("generated/models/item/${name}_slab.json").writeText(readFile("/models/item/slab.json").replace("::", name))
+        File("generated/blockstates/${name}_slab.json").writeText(readFile("/blockstates/slab.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/block/${name}_slab.json").writeText(readFile("/models/block/slab.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/block/${name}_slab_top.json").writeText(readFile("/models/block/slab_top.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/item/${name}_slab.json").writeText(readFile("/models/item/slab.json").replace("::", name).replace("namespace", namespace))
     } else {
-        File("generated/blockstates/${name}_stairs.json").writeText(readFile("/blockstates/stairs.json").replace("::", name))
-        File("generated/models/block/${name}_stairs.json").writeText(readFile("/models/block/stairs.json").replace("::", name))
-        File("generated/models/block/${name}_stairs_inner.json").writeText(readFile("/models/block/stairs_inner.json").replace("::", name))
-        File("generated/models/block/${name}_stairs_outer.json").writeText(readFile("/models/block/stairs_outer.json").replace("::", name))
-        File("generated/models/item/${name}_stairs.json").writeText(readFile("/models/item/stairs.json").replace("::", name))
+        File("generated/blockstates/${name}_stairs.json").writeText(readFile("/blockstates/stairs.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/block/${name}_stairs.json").writeText(readFile("/models/block/stairs.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/block/${name}_stairs_inner.json").writeText(readFile("/models/block/stairs_inner.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/block/${name}_stairs_outer.json").writeText(readFile("/models/block/stairs_outer.json").replace("::", name).replace("namespace", namespace))
+        File("generated/models/item/${name}_stairs.json").writeText(readFile("/models/item/stairs.json").replace("::", name).replace("namespace", namespace))
     }
 
 }
