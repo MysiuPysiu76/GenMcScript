3b2076a6b61c4a09c172e7d3a4e421702bfe3a86
Michał Hrycek <kotyk9921@gmail.com>
2025-08-03 02:29:24 +0200

    Added models for stairs
    Added models for stairs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,29 @@ package com.mysiupysiu.genmcscript
 import java.io.File
 
 fun main() {
-    val name = readLine() ?: ""
+    val values = readLine() ?: ""
+    val parts = values.trim().split(" ")
+
+    val type = parts[0]
+    val name = parts[1]
 
     File("generated/blockstates").mkdirs()
     File("generated/models/block").mkdirs()
     File("generated/models/item").mkdirs()
 
-    File("generated/blockstates/${name}_slab.json").writeText(readFile("/blockstates/slab.json").replace("::", name))
-    File("generated/models/block/${name}_slab.json").writeText(readFile("/models/block/slab.json").replace("::", name))
-    File("generated/models/block/${name}_slab_top.json").writeText(readFile("/models/block/slab_top.json").replace("::", name))
-    File("generated/models/item/${name}_slab.json").writeText(readFile("/models/item/slab.json").replace("::", name))
+    if (type == "slab") {
+        File("generated/blockstates/${name}_slab.json").writeText(readFile("/blockstates/slab.json").replace("::", name))
+        File("generated/models/block/${name}_slab.json").writeText(readFile("/models/block/slab.json").replace("::", name))
+        File("generated/models/block/${name}_slab_top.json").writeText(readFile("/models/block/slab_top.json").replace("::", name))
+        File("generated/models/item/${name}_slab.json").writeText(readFile("/models/item/slab.json").replace("::", name))
+    } else {
+        File("generated/blockstates/${name}_stairs.json").writeText(readFile("/blockstates/stairs.json").replace("::", name))
+        File("generated/models/block/${name}_stairs.json").writeText(readFile("/models/block/stairs.json").replace("::", name))
+        File("generated/models/block/${name}_stairs_inner.json").writeText(readFile("/models/block/stairs_inner.json").replace("::", name))
+        File("generated/models/block/${name}_stairs_outer.json").writeText(readFile("/models/block/stairs_outer.json").replace("::", name))
+        File("generated/models/item/${name}_stairs.json").writeText(readFile("/models/item/stairs.json").replace("::", name))
+    }
+
 }
 
 fun readFile(filename: String): String {
